9e2983edec8dc47dd96944476b7e52e5020dc1c1
aperepeliak <aperepelyak@gmail.com>
2016-10-10 13:35:50 +0300

    add collection to controller

--- LearningAngular/firstApp/app.js
+++ LearningAngular/firstApp/app.js
@@ -11,5 +11,59 @@ var myModule = angular.module('Angello', []);
 // Defining neccessary components for the application on the myModule property.
 myModule.factory('AngelloHelper', function () { });
 myModule.service('AngelloModel', function () { });
-myModule.controller('MainCtrl', function () { });
+
+myModule.controller('MainCtrl', function () {
+
+    /* 
+        It is better to store a reference to the top level 'this' object in case we need it later.
+        It is also better to name this the same name we declare the controller-as in the view.
+        This makes it easier to read and connect the dots as you jump between the HTML and JS.
+     */
+    var main = this;
+
+    main.stories = [
+        {
+            title: 'First Story',
+            description: 'Our first story.',
+            criteria: 'Criteria pending.',
+            status: 'To Do',
+            type: 'Feature',
+            reporter: 'Lukas Ruebbelke',
+            assignee: 'Brian Ford'
+        },
+        {
+            title: 'Second Story',
+            description: 'Do something.',
+            criteria: 'Criteria pending.',
+            status: 'Back Log',
+            type: 'Feature',
+            reporter: 'Lukas Ruebbelke',
+            assignee: 'Brian Ford'
+        },
+        {
+            title: 'Another story',
+            description: 'Just one more.',
+            criteria: 'Criteria pending.',
+            status: 'Code Review',
+            type: 'Enhancement',
+            reporter: 'Lukas Ruebbelke',
+            assignee: 'Brian Ford'
+        }
+    ];
+
+    main.createStory = function() {
+        main.stories.push({
+            title: 'New story',
+            description: 'Desc pending',
+            criteria: 'Criteria pending.',
+            status: 'Back Log',
+            type: 'Feature',
+            reporter: 'Pending',
+            assignee: 'Pending' 
+        });
+    };
+    
+});
+
+
 myModule.directive('story', function () { });
