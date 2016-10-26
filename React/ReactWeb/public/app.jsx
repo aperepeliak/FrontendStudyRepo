ffb3b57cdba553c3f7ae4b332c03dbec6acb13ed
aperepeliak <aperepelyak@gmail.com>
2016-10-26 12:54:39 +0300

    add component state example

--- React/ReactWeb/public/app.jsx
+++ React/ReactWeb/public/app.jsx
@@ -7,25 +7,52 @@ var Greeter = React.createClass({
         }
     },
 
+    getInitialState() {
+        return {
+            name: this.props.name
+        };
+    },
+
+    onButtonClick(event) {
+        // preventing the form from submiting and a page to refresh
+        event.preventDefault();
+
+        let nameRef = this.refs.name;
+        let name = nameRef.value;
+
+        nameRef.value = '';
+
+        if (typeof name === 'string' && name.length > 0) {
+            this.setState({
+                name: name
+            });
+        }
+    },
+
     render() {
 
-       let name = this.props.name;
-       let message = this.props.message;
+        let name = this.state.name;
+        let message = this.props.message;
 
-       return (
-          <div>
-            <h1>Hello {name}!</h1>
-            <p>{message + '!!'}</p>
-          </div>
-       );
-    } 
+        return (
+            <div>
+                <h1>Hello {name}!</h1>
+                <p>{message + '!!'}</p>
+
+                <form onSubmit={this.onButtonClick}>
+                    <input placeholder="Type your name" type="text" ref="name" />
+                    <button>Set Name</button>
+                </form>
+            </div>
+        );
+    }
 });
 
 let firstName = 'Andrew';
 let messageToPass = 'This is my message';
 
 ReactDOM.render(
-            <Greeter name={firstName} message={messageToPass} />,
-            document.getElementById('app')
-        );
+    <Greeter name={firstName} message={messageToPass} />,
+    document.getElementById('app')
+);
 
