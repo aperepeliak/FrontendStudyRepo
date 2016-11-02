df0816b9fbac4d4347308f893af4f62b463bffad
aperepeliak <aperepelyak@gmail.com>
2016-11-02 12:17:18 +0200

    add style to nav

--- React/ReactWeb/ReactTimer/app/components/Main.jsx
+++ React/ReactWeb/ReactTimer/app/components/Main.jsx
@@ -1,11 +1,13 @@
 /* jshint esversion: 6 */
 let React = require('react');
+let Nav = require('Nav');
 
 let Main = (props) => {
     return (
         <div>
             <div>
                 <div>
+                    <Nav />
                     <p>Main.jsx rendered</p>
                     {props.children}
                 </div>
---
+++ React/ReactWeb/ReactTimer/app/components/Nav.jsx
@@ -0,0 +1,28 @@
+/* jshint esversion: 6 */
+let React = require('react');
+let {Link, IndexLink} = require('react-router');
+
+let Nav = () => {
+    return (
+        <div className="top-bar">
+            <div className="top-bar-left">
+                <ul className="menu">
+                    <li className="menu-text">React Timer App</li>
+                    <li>
+                        <IndexLink to="/" activeClassName="active-link">Timer</IndexLink>
+                    </li>
+                    <li>
+                        <Link to="/" activeClassName="active-link">Countdown</Link>
+                    </li>
+                </ul>
+            </div>
+            <div className="top-bar-right">
+                <ul className="menu">
+                    <li className="menu-text">created by <a href="https://aperepeliak.github.io">aperepeliak</a></li>
+                </ul>
+            </div>
+        </div>
+    );
+};
+
+module.exports = Nav;
