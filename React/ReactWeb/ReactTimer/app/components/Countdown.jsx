7ac3579fc23764406d1ebf88767ebff0ae87a49a
aperepeliak <aperepelyak@gmail.com>
2016-11-03 14:31:42 +0200

    add countdown component

--- React/ReactWeb/ReactTimer/app/components/Countdown.jsx
+++ React/ReactWeb/ReactTimer/app/components/Countdown.jsx
@@ -1,12 +1,30 @@
 /* jshint esversion: 6 */
 let React = require('react');
 let Clock = require('Clock');
+let CountdownForm = require('CountdownForm');
 
 let Countdown = React.createClass({
+
+    getInitialState() {
+        return {
+            count: 0
+        }
+    },
+
+    handleSetCountdown(seconds) {
+        this.setState({
+            count: seconds
+        });
+    },
+
     render() {
+
+        let {count} = this.state;
+
         return (
             <div>
-                <Clock totalSeconds={129} />
+                <Clock totalSeconds={count} />
+                <CountdownForm onSetCountdown={this.handleSetCountdown}/>
             </div>
         );
     }
--- React/ReactWeb/ReactTimer/app/components/Main.jsx
+++ React/ReactWeb/ReactTimer/app/components/Main.jsx
@@ -5,14 +5,12 @@ let Nav = require('Nav');
 let Main = (props) => {
     return (
         <div>
-
             <Nav />
             <div className="row">
                 <div className="columns medium-6 large-4 small-centered">
                     {props.children}
                 </div>
             </div>
-
         </div>
     );
 };
