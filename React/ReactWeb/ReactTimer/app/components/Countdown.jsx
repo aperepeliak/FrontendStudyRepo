3d3484ee82a29668f2787ca9bcee6796aa77960a
aperepeliak <aperepelyak@gmail.com>
2016-11-03 15:49:51 +0200

    add countdown timer and state change

--- React/ReactWeb/ReactTimer/app/components/Countdown.jsx
+++ React/ReactWeb/ReactTimer/app/components/Countdown.jsx
@@ -7,13 +7,35 @@ let Countdown = React.createClass({
 
     getInitialState() {
         return {
-            count: 0
+            count: 0,
+            countdownStatus: 'stopped'
         }
+    },
+
+    // componentDidUpdate is called every time the state or props gets updated
+    componentDidUpdate(prevProps, prevState) {
+        if (this.state.countdownStatus !== prevState.countdownStatus) {
+            switch (this.state.countdownStatus) {
+                case 'started':
+                    this.startTimer();
+                    break;
+            }
+        }
+    },
+
+    startTimer() {
+        this.timer = setInterval(() => {
+            let newCount = this.state.count - 1;
+            this.setState({
+                count: newCount >=0 ? newCount : 0
+            });
+        }, 1000);
     },
 
     handleSetCountdown(seconds) {
         this.setState({
-            count: seconds
+            count: seconds,
+            countdownStatus: 'started'
         });
     },
 
@@ -24,7 +46,7 @@ let Countdown = React.createClass({
         return (
             <div>
                 <Clock totalSeconds={count} />
-                <CountdownForm onSetCountdown={this.handleSetCountdown}/>
+                <CountdownForm onSetCountdown={this.handleSetCountdown} />
             </div>
         );
     }
