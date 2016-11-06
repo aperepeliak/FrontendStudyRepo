d7b4109198d5cd76b626ecda18f8a8f25774ea73
aperepeliak <aperepelyak@gmail.com>
2016-11-06 13:17:01 +0200

    add new react boilerplate with testing section

---
+++ React/ReactWeb/ReactBoilerplate3/app/app.jsx
@@ -0,0 +1,15 @@
+/* jshint esversion:6 */
+let React = require('react');
+let ReactDOM = require('react-dom');
+let {Route, Router, IndexRoute, hashHistory} = require('react-router');
+
+// Load foundation
+$(document).foundation();
+
+// App css
+require('style!css!sass!applicationStyles')
+
+ReactDOM.render(
+  <p>Boilerplate 3 Project</p>,
+  document.getElementById('app')
+);
