6b8136df30a2e7e2537c4144a5aadbd19eae9d60
aperepeliak <aperepelyak@gmail.com>
2016-11-07 20:47:26 +0200

    init reactTodo app

---
+++ React/ReactWeb/ReactTodo/app/components/TodoApp.jsx
@@ -0,0 +1,14 @@
+/* jshint esversion:6 */
+let React = require('react');
+
+let TodoApp = React.createClass({
+    render() {
+        return (
+            <div>
+                Todo.jsx
+            </div>
+        );
+    }
+});
+
+module.exports = TodoApp;
