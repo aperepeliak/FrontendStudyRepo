d468149b29513aa1f765ed071e6fd91e57c7c4ba
aperepeliak <aperepelyak@gmail.com>
2016-11-10 11:38:06 +0200

    starting learning redux

---
+++ React/ReactWeb/LearningRedux/app/app.jsx
@@ -0,0 +1,17 @@
+/* jshint esversion:6 */
+let React = require('react');
+let ReactDOM = require('react-dom');
+let {Route, Router, IndexRoute, hashHistory} = require('react-router');
+
+// Load foundation
+$(document).foundation();
+
+// App css
+require('style!css!sass!applicationStyles')
+
+ReactDOM.render(
+  <p>Boilerplate 3 Project</p>,
+  document.getElementById('app')
+);
+
+require('./redux-example.jsx');
---
+++ React/ReactWeb/LearningRedux/app/redux-example.jsx
@@ -0,0 +1,4 @@
+/* jshint esversion: 6 */
+let redux = require('redux');
+
+console.log('Starting redux example');
