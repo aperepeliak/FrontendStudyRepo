23d2bfa28741b34ac1b11c048d18cf4e7026deb7
aperepeliak <aperepelyak@gmail.com>
2016-11-19 18:09:06 +0200

    add first two reducers and tests for them

---
+++ React/ReactWeb/ReactTodoRedux/app/reducers/reducers.jsx
@@ -0,0 +1,26 @@
+/* jshint esversion: 6 */
+
+export let searchTextReducer = (state = '', action) => {
+    switch (action.type) {
+
+        case 'SET_SEARCH_TEXT':
+            return action.searchText;
+
+        default:
+            return state;
+    }
+};
+
+export let showCompletedReducer = (state = false, action) => {
+    switch(action.type) {
+
+        case 'TOGGLE_SHOW_COMPLETED':
+            return !state;
+
+        default:
+            return state;
+    }
+};
+
+
+
