1fec622be7741c36f59da900612272540fc7056c
aperepeliak <aperepelyak@gmail.com>
2016-11-24 12:55:56 +0200

    learning to remove data from firebase

--- React/ReactWeb/ReactTodoFirebase/playground/index.js
+++ React/ReactWeb/ReactTodoFirebase/playground/index.js
@@ -12,48 +12,68 @@ firebase.initializeApp(config);
 
 let firebaseRef = firebase.database().ref();
 
-// Setting data on the database
-// We can add everything bu arrays
-// set() erases all the previous data
-// set() also returns a Promise
-firebaseRef.set({
-    app: {
-        name: 'Todo App',
-        version: '1.0.0'
-    },
-    isRunning: true,
-    user: {
-        name: 'Andrii',
-        age: 27
-    }, 
-});
+/* Setting data 
+    // Setting data on the database
+    // We can add everything bu arrays
+    // set() erases all the previous data
+    // set() also returns a Promise
+*/
+    firebaseRef.set({
+        app: {
+            name: 'Todo App',
+            version: '1.0.0'
+        },
+        isRunning: true,
+        user: {
+            name: 'Andrii',
+            age: 27
+        }, 
+    });
 
-// firebaseRef.child('app').set({
-//     name: 'Todo application'
-// });
+ //   firebaseRef.child('app').set({
+ //       name: 'Todo application'
+ //   });
 
-// update() modifies existing data without erasing other data
-// multipath updates --> 'app/name': value || or to use child()
 
-// update() comes with a promise as its return value
-// firebaseRef.update({
-//     isRunning: false,
-//     'app/name' : 'Todo application'
-// });
+/* Data modification 
+    update() modifies existing data without erasing other data
+    multipath updates --> 'app/name': value || or to use child()
 
-// firebaseRef.child('app').update({
-//     name: 'My Todo App'
-// });
+    update() comes with a promise as its return value
+    firebaseRef.update({
+        'app/name': 'Todo application',
+        'user/name': 'Vasya'
+    });
+    firebaseRef.child('app').update({
+        name: 'Todo application'
+    });
+    firebaseRef.child('user').update({
+        name: 'Misha'
+    });
+*/
 
-// firebaseRef.update({
-//     'app/name': 'Todo application',
-//     'user/name': 'Vasya'
-// });
+/* Removing data 
+    // wipe the db completely. Also returns promise
+    // firebaseRef.remove();
 
-firebaseRef.child('app').update({
-    name: 'Todo application'
-});
-firebaseRef.child('user').update({
-    name: 'Misha'
-});
+    // remove only app object
+    // firebaseRef.child('app').remove(); 
+
+    // remove particular field in a nested object
+    // firebaseRef.child('app/name').remove();
+
+    // setting value to null
+    // firebaseRef.child('app').update({
+    //     version: '2.0.0',
+    //     name: null
+    // });
+
+    // Two more examples
+    //  firebaseRef.update({
+    //      isRunning: null
+    //  });
+    // firebaseRef.child('user/age').remove();
+*/
+
+
 
