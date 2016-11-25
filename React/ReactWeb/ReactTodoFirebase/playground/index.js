59604bf1f2cef3d2eaa0f060ac7d0640a6f005b9
aperepeliak <aperepelyak@gmail.com>
2016-11-25 15:40:43 +0200

    learning firebase events and working with arrays

--- React/ReactWeb/ReactTodoFirebase/playground/index.js
+++ React/ReactWeb/ReactTodoFirebase/playground/index.js
@@ -14,7 +14,7 @@ let firebaseRef = firebase.database().ref();
 
 /* Setting data 
     // Setting data on the database
-    // We can add everything bu arrays
+    // We can add everything but arrays
     // set() erases all the previous data
     // set() also returns a Promise
 */
@@ -75,50 +75,155 @@ let firebaseRef = firebase.database().ref();
     // firebaseRef.child('user/age').remove();
 */
 
-/* Fetching data */
+/* Fetching data 
 
-// snapshot has props: key, values
-// val() returns root object (the whole db)
-firebaseRef.once('value').then((snapshot) => {
-    console.log('Got entire database', snapshot.val());
-}, (e) => {console.log('unable to feth the data: ', e);});
+    // // snapshot has props: key, values
+    // // val() returns root object (the whole db)
+    // firebaseRef.once('value').then((snapshot) => {
+    //     console.log('Got entire database', snapshot.val());
+    // }, (e) => {console.log('unable to feth the data: ', e);});
 
-// to fetch the subset of db call child()
-firebaseRef.child('app').once('value').then((snapshot) => {
-    console.log('Key\t\t:', snapshot.key);
-    console.log('Value\t:', snapshot.val());
-}, (e) => {console.log('unable to feth the data: ', e);});
+    // // to fetch the subset of db call child()
+    // firebaseRef.child('app').once('value').then((snapshot) => {
+    //     console.log('Key\t\t:', snapshot.key);
+    //     console.log('Value\t:', snapshot.val());
+    // }, (e) => {console.log('unable to feth the data: ', e);});
 
-// listening for changes
-// we can use on('value', ...) with root or any child reference
-// firebaseRef.on('value', (snapshot) => {
-//     console.log('Got value:', snapshot.val());
-// });
+    // // listening for changes
+    // // we can use on('value', ...) with root or any child reference
+    // firebaseRef.on('value', (snapshot) => {
+    //     console.log('Got value:', snapshot.val());
+    // });
 
-// removing all listeners
-// firebaseRef.off();
+    // // removing all listeners
+    // // firebaseRef.off();
 
-// better to define callbacks in separate function vars in order to be able to
-// add several listeners and be able to remove particular one if needed. Like this:
-// let logData = (snapshot) => {
-//     console.log('Got value:', snapshot.val());
-// };
+    // // better to define callbacks in separate function vars in order to be able to
+    // // add several listeners and be able to remove particular one if needed. Like this:
+    // let logData = (snapshot) => {
+    //     console.log('Got value:', snapshot.val());
+    // };
 
-// firebaseRef.on('value', logData);
-// firebaseRef.off('value', logData);
+    // firebaseRef.on('value', logData);
+    // firebaseRef.off('value', logData);
 
-// mini-challenge
-let userChange = (snapshot) => {
-    console.log('User changed: ', snapshot.val());
-};
-firebaseRef.child('user').on('value', userChange);
+*/
 
-firebaseRef.child('user').update({
-    name: 'Sveta'
+/* mini-challenge
+    let userChange = (snapshot) => {
+        console.log('User changed: ', snapshot.val());
+    };
+    firebaseRef.child('user').on('value', userChange);
+
+    firebaseRef.child('user').update({
+        name: 'Sveta'
+    });
+    firebaseRef.child('app').update({
+        name: 'Application Todo'
+    });
+*/
+
+
+/* Working with arrays */
+// Instead of arrays firebase stores data in object with unique key
+    /* Example
+        // false
+        firebaseRef.set({
+            todos : [
+                {
+                    id: '123',
+                    text: 'do smth'
+                }
+            ]
+        });
+
+        // OK!
+        firebaseRef.set({
+            todos: {
+                '123' : {
+                    text: 'do smth'
+                }
+            }
+        });
+     */
+
+
+let notesRef = firebaseRef.child('notes');
+
+/* Full notation
+    let newNoteRef = notesRef.push();
+    newNoteRef.set({
+        text: 'Walk the dog'
+    });
+*/
+// Shorthand notation
+let newNoteRef = notesRef.push({
+    text: 'Wash the dishes'
 });
-firebaseRef.child('app').update({
-    name: 'Application Todo'
-});
+
+// Get the unique ID
+//console.log('Todo ID: ', newNoteRef.key);
+
+/* Other available events:
+    for new items       (CHILD_ADDED)   - fires every time you add new child to reference
+    for changed items   (CHILD_CHANGED) - 
+    for removed items   (CHILD_REMOVED) -
+ */
+
+/* Examples 
+
+    notesRef.on('child_added', (snapshot) => {
+        console.log('New note added: ');
+        console.log(' --> Key: ', snapshot.key);
+        console.log(' --> Val: ', snapshot.val());
+        console.log('--------------');
+    });
+
+    notesRef.on('child_changed', (snapshot) => {
+        console.log('Note changed: ');
+        console.log(' --> Key: ', snapshot.key);
+        console.log(' --> Val: ', snapshot.val());
+        console.log('--------------');
+    });
+
+    notesRef.on('child_removed', (snapshot) => {
+        console.log('Note removed: ');
+        console.log(' --> Key: ', snapshot.key);
+        console.log(' --> Val: ', snapshot.val());
+        console.log('--------------');
+    });
+
+*/
+
+/* Mini-challenge
+
+    let todosRef = firebaseRef.child('todos');
+
+    todosRef.on('child_added', (snapshot) => {
+        console.log('New todo added:');
+        console.log('\t=> Key: ', snapshot.key);
+        console.log('\t=> Val: ', snapshot.val());
+    });
+
+    todosRef.push({
+        text: 'Todo #1'
+    });
+        
+    todosRef.push({
+        text: 'Todo #2'
+    });
+ */
+
+
+
+
+
+
+
+
+
+
+
 
 
 
