b8a44536ad236891ac4654a1ee55a407724dac13
aperepeliak <aperepelyak@gmail.com>
2016-12-06 20:58:50 +0200

    remove db credentials

--- React/ReactWeb/ReactTodoFirebase/app/firebase/index.js
+++ React/ReactWeb/ReactTodoFirebase/app/firebase/index.js
@@ -3,11 +3,7 @@ import firebase from 'firebase';
 
 try {
     var config = {
-        apiKey: "AIzaSyBVFz5LfIr6Jr03FWorF2npAuACd9RvJ30",
-        authDomain: "todofireapp-3724f.firebaseapp.com",
-        databaseURL: "https://todofireapp-3724f.firebaseio.com",
-        storageBucket: "todofireapp-3724f.appspot.com",
-        messagingSenderId: "834731709312"
+        // db credentials
     };
 
     firebase.initializeApp(config);
