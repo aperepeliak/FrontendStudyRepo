fbb515a488729ab85aa2bf4e6a88dd10b7fe20b4
aperepeliak <aperepelyak@gmail.com>
2016-12-13 15:08:23 +0200

    init project for es6 classes

---
+++ React/ReactWeb/ES6 features/es6Classes/app/app.jsx
@@ -0,0 +1,18 @@
+/* jshint esversion:6 */
+let React = require('react');
+let ReactDOM = require('react-dom');
+let {Route, Router, IndexRoute, hashHistory} = require('react-router');
+
+import ComponentOne from './components/ComponentOne';
+import ComponentTwo from './components/ComponentTwo';
+
+// Load foundation
+$(document).foundation();
+
+// App css
+require('style!css!sass!applicationStyles')
+
+ReactDOM.render(
+  <p>Boilerplate 3 Project</p>,
+  document.getElementById('app')
+);
---
+++ React/ReactWeb/ES6 features/es6Classes/app/components/ComponentOne.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+let ComponentOne = React.createClass({
+
+    render() {
+        return (
+            <div>
+                <h3>Component One using React.createClass</h3>
+            </div>
+        );
+
+    }
+});
+
+export default ComponentOne;
